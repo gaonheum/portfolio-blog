d99e931187a4aee3f524f323121fa7fe9a884b1f
mssso-dev <msseo.dev@gmail.com>
2022-01-26 17:25:25 +0900

    tailwind.config.js -> purge
    modified src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: [
     './public/**/*.html',
-    './src/**/*.vue',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
