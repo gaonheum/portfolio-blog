68dade5206acb34b9b39049917e8ad01174bb0b3
mssso-dev <msseo.dev@gmail.com>
2022-01-26 19:10:53 +0900

    vuex store modularization

---
+++ src/store/headerStore.js
@@ -0,0 +1,25 @@
+
+const headerStore = {
+  namespaced: true,
+  state: {      // vue의 Data 기능
+    mainMenu: [
+      { title: "Home", to: '/' },
+      { title: "Blog", to: '/blog' },
+      { title: "Portfolios", to: '/portfolios' },
+      { title: "Tags", to: '/tags'  },
+      { title: "Archives", to: '/archives'  },
+      { title: "About", to: '/about'  },
+    ]
+  },
+  getters: {    // vue의 computed 기능
+
+  },
+  mutations: {  // state를 동기적으로 수정할 때 사용, state 수정 시, mutations를 통해 수정하는 것을 권장 -> 데이터를 추적할 수 있음
+
+  },
+  actions: {    // 비동기를 사용할 때 또는, 여러 mutations를 연달아 실행할 때 사용
+
+  },
+}
+
+export default headerStore;
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,9 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+import headerStore from './headerStore';
+import sidebarStore from './sidebarStore';
+
 // Object Dynamic property: mutations의 함수 이름을 변수로 선언하여, 다른 파일에서 사용할 수 있게 함
 /*
 * 여기에 작성
@@ -14,17 +17,12 @@ export const hPORTFOLIO = "Portfolio";
 export const hABOUT     = "About";
 
 export const store = new Vuex.Store({
-  state: {      // vue의 Data 기능
-
+  modules: {
+    // 키: 값 형태로 저장
+    headerStore,
+    sidebarStore,
   },
-  getters: {    // vue의 computed 기능
-
-  },
-  mutations: {  // state를 동기적으로 수정할 때 사용, state 수정 시, mutations를 통해 수정하는 것을 권장 -> 데이터를 추적할 수 있음
-
-  },
-  actions: {    // 비동기를 사용할 때 또는, 여러 mutations를 연달아 실행할 때 사용
-
-  },
-
+  state: {
+    mobile: false,
+  }
 });
