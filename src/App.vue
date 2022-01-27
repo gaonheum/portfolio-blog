68a0ccc2c6d9b94cab1db0a5e2ef9e303f3c3111
mssso-dev <msseo.dev@gmail.com>
2022-01-27 20:06:26 +0900

    header, sidebar, content, footer ver.1.0

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,172 @@
 <template>
-  <div id="app">
-    <img
-      alt="Vue logo"
-      src="./assets/logo.png"
-    >
+  <div
+    id="app"
+  >
+    <div class="layout">
+      <!-- Sidebar -->
+      <aside
+        v-show="sidebar"
+        :class="openSidebar"
+        class="left-side"
+      >
+        <sidebar-menu />
+      </aside>
+
+      <div class="flexbox">
+        <!-- Header -->
+        <div class="header-basis bg-gray-800">
+          <div class="header-basis-left" />
+          <div class="header">
+            <main-header />
+          </div>
+          <div class="header-basis-right" />
+        </div>
+
+        <div class="content-basis">
+          <div class="content-basis-left" />
+
+          <!-- Content -->
+          <div class="content">
+            <router-view />
+          </div>
+
+
+          <div class="content-basis-right" />
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <div class="footer">
+      <main-footer />
+    </div>
   </div>
 </template>
 
 <script>
 
+import { mapState, mapMutations } from 'vuex';
+
+import MainHeader from "@/components/header/MainHeader";
+import MainFooter from "@/components/footer/MainFooter";
+import SidebarMenu from "@/components/sidebar/SidebarMenu";
+
 export default {
   name: 'App',
   components: {
-  }
+    'sidebar-menu': SidebarMenu,
+    'main-header': MainHeader,
+    'main-footer': MainFooter,
+  },
+  data: () => {
+    return {
+
+    };
+  },
+  computed: {
+    ...mapState(['width', 'sidebar',]),
+    openSidebar() {
+      if (this.sidebar === true) {
+        return "display: block";
+      } else {
+        return "display: none";
+      }
+    }
+  },
+  created() {
+    window.addEventListener('load', this.curView);
+  },
+  mounted() {
+    window.addEventListener('resize', this.mobileView);
+  },
+  beforeDestroy() {
+    window.removeEventListener('load', this.curView);
+    window.removeEventListener('resize', this.mobileView);
+  },
+  methods: {
+    ...mapMutations(["curView", "mobileView",]),
+  },
 }
 </script>
 
 <style>
+body {
+  background-color: gray;
+
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /*width: 100%;*/
+  /*height: 100%;*/
+
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+}
+
+.layout {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: nowrap;
+  background-color: beige;
+}
+
+.left-side {
+  flex-basis: 300px;
+  flex-grow: 0;
+  flex-shrink: 0;
+}
+
+.flexbox {
+  padding: 0 0;
+}
+
+.header-basis {
+  display: flex;
+  padding: 0 10px;
+}
+
+.content-basis {
+  padding: 10px;
+  display: flex;
+  flex-grow: 0;
+  flex-shrink: 0;
+}
+
+.content {
+  padding: 15px 0;
+  width: 100%;
+  height: 100%;
+}
+
+@media (max-width: 1536px) {
+  .content-basis-left,
+  .content-basis-right,
+  .header-basis-left,
+  .header-basis-right {
+    flex-basis: 2.5%;
+  }
+  .content,
+  .header {
+    flex-basis: 95%;
+  }
+}
+@media (min-width: 1536px) {
+  .content-basis-left,
+  .content-basis-right,
+  .header-basis-left,
+  .header-basis-right {
+    flex-basis: 7.5%;
+  }
+  .content,
+  .header {
+    flex-basis: 85%;
+  }
 }
 </style>
---
+++ src/views/Blog.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <article>
+      <div class="item bg-red-200">
+        content1
+      </div>
+      <div class="item bg-yellow-200">
+        content2
+      </div>
+      <div class="item bg-green-200">
+        content3
+      </div>
+      <div class="item bg-blue-200">
+        content4
+      </div>
+      <router-view />
+    </article>
+
+    <!-- Recent Update / Table of contents -->
+    <aside class="right-side text-center">
+      <b>Table of contents</b>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Blog"
+}
+</script>
+
+<style scoped>
+article {
+  padding: 0 0.5em;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-direction: column;
+}
+
+.item {
+  min-height: 150px;
+  flex-basis: 150px;
+  flex-grow: 1;
+  flex-shrink: 1;
+  text-align: center;
+}
+
+.right-side {
+  margin: 0 0.5em;
+  padding: 10px;
+  width: 100%;
+  height: 100%;
+  flex-basis: 220px;
+  flex-grow: 0;
+  flex-shrink: 0;
+  background-color: lawngreen;
+}
+
+@media (max-width: 1280px) {
+  .right-side {
+    display: none;
+  }
+}
+</style>
